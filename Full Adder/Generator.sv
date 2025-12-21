a082dc0085f17847a31ea99077d207382fa68064
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2025-12-21 13:03:53 +0530

    Create Generator.sv

---
+++ Full Adder/Generator.sv
@@ -0,0 +1,23 @@
+`include "transaction.sv"
+class generator;
+  transact trans;
+  mailbox m1;
+  function new(mailbox m1);
+    this.m1=m1;
+  endfunction
+  
+  task main();
+ 
+    repeat(3)
+      begin
+      trans=new();
+      trans.randomize();
+      
+      m1.put(trans);
+      trans.display("Generator Class");
+    end
+  endtask
+endclass
+
+    
+      
