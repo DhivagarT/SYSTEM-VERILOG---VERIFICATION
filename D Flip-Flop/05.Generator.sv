b577b9e2c7fcd5467b81bfe05ae013e7bd5e0ba2
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2025-12-23 22:06:25 +0530

    Create 05.Generator.sv

---
+++ D Flip-Flop/05.Generator.sv
@@ -0,0 +1,17 @@
+class generator;
+  transaction trans;
+  mailbox m1;
+  
+  function new(mailbox m1);
+    this.m1=m1;
+  endfunction
+  
+  task main();
+    repeat(3)begin
+      trans=new();
+      trans.randomize();
+      m1.put(trans);
+      trans.display("Generator Class");
+    end
+  endtask
+endclass
