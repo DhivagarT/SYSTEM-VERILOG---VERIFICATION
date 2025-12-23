f84dbf86b1e52f2cd1959e6f10ed92206ce3dbb5
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2025-12-23 22:09:38 +0530

    Create Output.sv

---
+++ D Flip-Flop/Output.sv
@@ -0,0 +1,28 @@
+# KERNEL: ----Generator Class----
+# KERNEL: Time=0  Rst=0 D=1 Q=0
+# KERNEL: ----Generator Class----
+# KERNEL: Time=0  Rst=1 D=0 Q=0
+# KERNEL: ----Generator Class----
+# KERNEL: Time=0  Rst=1 D=1 Q=0
+# KERNEL: ----Driver Class----
+# KERNEL: Time=2  Rst=0 D=1 Q=0
+# KERNEL: ----Monitor Class----
+# KERNEL: Time=3  Rst=0 D=1 Q=0
+# KERNEL: ---PASS---
+# KERNEL: ----Scoreboard Class----
+# KERNEL: Time=3  Rst=0 D=1 Q=0
+# KERNEL: ----Driver Class----
+# KERNEL: Time=6  Rst=1 D=0 Q=0
+# KERNEL: ----Monitor Class----
+# KERNEL: Time=7  Rst=1 D=0 Q=0
+# KERNEL: ---PASS---
+# KERNEL: ----Scoreboard Class----
+# KERNEL: Time=7  Rst=1 D=0 Q=0
+# KERNEL: ----Driver Class----
+# KERNEL: Time=10  Rst=1 D=1 Q=0
+# KERNEL: ----Monitor Class----
+# KERNEL: Time=11  Rst=1 D=1 Q=0
+# KERNEL: ---PASS---
+# KERNEL: ----Scoreboard Class----
+# KERNEL: Time=11  Rst=1 D=1 Q=0
+# RUNTIME: Info: RUNTIME_0068 $finish called.
